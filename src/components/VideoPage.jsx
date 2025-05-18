1fca5eb45bcde5e2fa4634e27251b38c01b65226
Abhinav-378 <abhichoukse05@gmail.com>
2025-05-18 15:09:11 +0530

    like functionality added in video page

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -11,6 +11,7 @@ function VideoPage() {
   const [channel, setChannel] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [liked, setLiked] = useState(false);
   const navigate = useNavigate();
   const API_URL =
     import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1";
@@ -24,7 +25,7 @@ function VideoPage() {
         },
       });
       setVideo(response.data.data);
-      console.log("video", response.data.data);
+      // console.log("video", response.data.data);
     } catch (error) {
       setError(error.response.data.message || "Something went wrong");
       console.error("Error fetching video:", error);
@@ -43,13 +44,66 @@ function VideoPage() {
           },
         }
       );
-      console.log("channel", response.data.data);
+      // console.log("channel", response.data.data);
       setChannel(response.data.data);
     } catch (error) {
       setError(error.response.data.message || "Something went wrong");
       console.error("Error fetching channel:", error);
     }
   };
+  const checkLike = async (videoId) => {
+    if(!user) {
+      console.log("User not logged in for like check");
+      return;
+    }
+    try {
+      const response = await axios.get(
+        `${API_URL}/likes/video/${videoId}`,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      // console.log("like", response.data.data);
+      setLiked(response.data.data);
+      
+    } catch (error) {
+      setError(error.response.data.message || "Something went wrong");
+      console.error("Error fetching channel:", error);
+    }
+  };
+  const toggleLike = async (videoId) => {
+    if (!user) {
+      alert("Please login to like the video");
+      return;
+    }  
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        `${API_URL}/likes/toggle/v/${videoId}`,
+        {},
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+      // console.log("like", response.data.data);
+      setLiked((prev) => !prev);
+      setVideo((prev) => ({
+        ...prev,
+        likes: prev.likes + (liked ? -1 : 1),
+      }));
+    } catch (error) {
+      setError(error.response.data.message || "Something went wrong");
+      console.error("Error fetching channel:", error);
+    }
+    finally {
+      setLoading(false);
+    }
+  }
   const toggleSubscription = async () => {
     if (!user) {
       navigate("/login");
@@ -115,14 +169,19 @@ function VideoPage() {
     return `${Math.floor(diff / 31536000)} years ago`;
   }
   useEffect(() => {
-    fetchVideo();
-    addtoWatchHistory();
-  }, [videoId]);
+  fetchVideo();
+  addtoWatchHistory();
+}, []);
+
+// Second useEffect for channel fetch
+useEffect(() => {
+  if (video) {
+    fetchChannel(video.owner.username);
+  }
+}, [video]);
   useEffect(() => {
-    if (video) {
-      fetchChannel(video.owner.username);
-    }
-  }, [video]);
+    checkLike(videoId);
+  }, [user]);
   return (
     <div className="text-white">
       {loading && (
@@ -158,6 +217,22 @@ function VideoPage() {
                 </p>
               </div>
             </div>
+            <div className="flex flex-row justify-center items-center gap-2">
+              <div onClick={() => {toggleLike(video?._id)}} className="flex flex-col justify-center items-center ">
+                {
+                  liked
+                  ?
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 cursor-pointer">
+                  <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                </svg>
+                  :
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                </svg>
+                }
+                {video?.likes} Likes
+              </div>
+            
             {user?.username === channel?.username ? (
               <button
                 className="bg-[#4d4d4d] text-white rounded-full px-4 py-2 hover:bg-[#343434]"
@@ -184,6 +259,7 @@ function VideoPage() {
                 )}
               </div>
             )}
+            </div>
           </div>
           <div className="w-full bg-[#303030] p-3 rounded-2xl my-3">
             <p className="text-base font-semibold text-gray-300 ">
