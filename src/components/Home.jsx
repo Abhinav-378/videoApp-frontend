afadff05d1fc9a842fc8fba3ef260c67712fad4f
Abhinav-378 <abhichoukse05@gmail.com>
2025-05-23 15:58:11 +0530

    added channel link, and time uploaded in each video

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,6 +22,19 @@ function Home() {
       setLoading(false);
     }
   };
+  function timeAgo(dateString) {
+    const now = new Date();
+    const date = new Date(dateString);
+    const diff = Math.floor((now - date) / 1000); // in secs
+
+    if (diff < 60) return `${diff} seconds ago`;
+    if (diff < 3600) return `${Math.floor(diff / 60)} minutes ago`;
+    if (diff < 86400) return `${Math.floor(diff / 3600)} hours ago`;
+    if (diff < 2592000) return `${Math.floor(diff / 86400)} days ago`;
+    if (diff < 31536000) return `${Math.floor(diff / 2592000)} months ago`;
+    return `${Math.floor(diff / 31536000)} years ago`;
+  }
+  
   useEffect(() => {
     fetchRandomVideos()
   }, [])
@@ -35,16 +48,27 @@ function Home() {
         <div className='flex flex-col md:flex-row flex-wrap items-start justify-start w-full mt-5'>
             {
                 videos.map((video)=>(
-                    <Link className='flex flex-col w-full md:w-1/3 justify-start items-start gap-4 p-4 ' to={`/watch/${video._id}`} key={video._id}>
+                  <div  className='flex flex-col w-full md:w-1/3 justify-start items-start gap-1 p-4 ' key={video._id}>
+                    <Link to={`/watch/${video._id}`} key={video._id}>
                         <div className='relative'>
                             <img src={video.thumbnail} alt={video.title} className=" rounded-lg w-dvw h-auto  " />
                             <div className='absolute bottom-2 right-2 rounded-md bg-black/70 text-white px-2 py-1 text-sm'>{video.duration}</div>          
                         </div>
                         <div className="channel-info">
-                            <h2 className="text-base text-white line-clamp-2">{video.title}</h2>
-                            <p className='text-sm text-gray-400'>{video.views} views</p>
+                            <h2 className="text-base text-white line-clamp-2 mt-2">{video.title}</h2>
                         </div>
                     </Link>
+                    <Link to={`/channel/${video.owner.username}`} >
+                      <div className='text-sm text-gray-400 hover:text-gray-200'>
+                      {video.owner.fullName}
+                      </div>
+                    </Link>
+                    <Link to={`/watch/${video._id}`} >
+                      <div className='text-sm text-gray-400'>
+                      {video.views} views • {timeAgo(video.createdAt)}
+                      </div>
+                    </Link>
+                    </div>
                     )
                 )
             }
