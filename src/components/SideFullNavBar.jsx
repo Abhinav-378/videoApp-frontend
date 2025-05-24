18cdd941c554a0f94be81aeb4c990cebcc92617d
Abhinav-378 <abhichoukse05@gmail.com>
2025-05-24 13:32:31 +0530

    miniNav fixed for mobile view

--- src/components/SideFullNavBar.jsx
+++ src/components/SideFullNavBar.jsx
@@ -1,142 +1,144 @@
-import React from "react";
+import React, { use } from "react";
 // import { HomeIcon } from '@heroicons/react/outline'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../AuthContext";
 function SideFullNavBar({ isOpen, onToggle }) {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const handleChannelClick = () => {
+    if (!user) {
+      alert("Please login to access this feature");
+      return;
+    }
+    navigate(`/channel/${user.username}/playlists`);
+  };
   return (
     <div
-      className={`h-screen bg-[#141414] fixed left-0 top-0 
-      transition-all duration-300 text-white ${isOpen ? "w-64" : "w-20"}`}
+      className={`sm:h-screen bg-[#141414] fixed left-0 sm:top-0 bottom-0
+      transition-all duration-300 text-white ${isOpen ? "w-56" : "sm:w-20 w-full"}`}
     >
-      <nav className={`mt-16 ${isOpen ? "px-4" : "px-2"}`}>
-        <div
-          className={`flex items-center gap-4 py-2 ${
-            !isOpen && "justify-center"
-          }`}
-        >
-          {/* home logo */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-7"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-            />
-          </svg>
-
-          {isOpen && (
-            <span
-              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${
-                isOpen ? "opacity-100 w-auto ml-2" : "opacity-0 w-0"
+      <nav className={`flex flex-row justify-around sm:flex-col sm:mt-16 ${isOpen ? "px-4" : "px-2"}`}>
+        <Link to="/">
+          <div
+            className={`flex  ${isOpen ? "flex-row items-center gap-4" : "flex-col items-center gap-1 "} }  py-2 px-1 hover:bg-[#242424] rounded-xl ${!isOpen && "justify-center"
               }`}
+          >
+            {/* home logo */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+
+
+            <span
+              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${isOpen ? "w-auto ml-2" : "w-auto text-xs"
+                }`}
             >
               Home
             </span>
-          )}
-        </div>
-        <Link to="/liked">
-        <div
-          className={`flex items-center gap-4 py-2 hover:bg-[#242424] rounded-xl ${
-            !isOpen && "justify-center"
-          }`}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-7"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z"
-            />
-          </svg>
 
-          {isOpen && (
-            <span
-              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${
-                isOpen ? "opacity-100 w-auto ml-2" : "opacity-0 w-0"
+          </div>
+        </Link>
+        <Link to="/liked">
+          <div
+            className={`flex ${isOpen ? "flex-row items-center gap-4" : "flex-col items-center gap-1 "} }  py-2 px-1 hover:bg-[#242424] rounded-xl ${!isOpen && "justify-center"
               }`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-7"
             >
-              Liked Videos
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z"
+              />
+            </svg>
+
+            <span
+              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${isOpen ? "w-auto ml-2" : "w-auto text-xs"
+                }`}
+            >
+              Liked
             </span>
-          )}
-        </div>
+
+          </div>
         </Link>
         <Link to="/history">
-        <div
-          className={`flex items-center gap-4 py-2 hover:bg-[#242424] rounded-xl ${
-            !isOpen && "justify-center"
-          }`}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-7"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-
-          {isOpen && (
-            <span
-              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${
-                isOpen ? "opacity-100 w-auto ml-2" : "opacity-0 w-0"
+          <div
+            className={`flex ${isOpen ? "flex-row items-center gap-4" : "flex-col items-center gap-1 "} }  py-2 px-1 hover:bg-[#242424] rounded-xl ${!isOpen && "justify-center"
               }`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+
+            <span
+              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${isOpen ? "w-auto ml-2" : "w-auto text-xs"
+                }`}
             >
               History
             </span>
-          )}
-        </div>
+          </div>
         </Link>
-        <div
-          className={`flex items-center gap-4 py-2 ${
-            !isOpen && "justify-center"
-          }`}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-7"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
-            />
-          </svg>
-
-          {isOpen && (
-            <span
-              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${
-                isOpen ? "opacity-100 w-auto ml-2" : "opacity-0 w-0"
+        <Link to="/dashboard" >
+          <div
+            className={`flex ${isOpen ? "flex-row items-center gap-4" : "flex-col items-center gap-1 "} }  py-2 px-1 hover:bg-[#242424] rounded-xl ${!isOpen && "justify-center"
               }`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
+              />
+            </svg>
+
+            <span
+              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${isOpen ? "w-auto ml-2" : "w-auto text-xs"
+                }`}
             >
               My Content
             </span>
-          )}
-        </div>
+          </div>
+        </Link>
         <div
-          className={`flex items-center gap-4 py-2 ${
-            !isOpen && "justify-center"
-          }`}
+            className={`hidden sm:flex ${isOpen ? "flex-row items-center gap-4" : "flex-col items-center gap-1 "} }  py-2 px-1 hover:bg-[#242424] rounded-xl ${!isOpen && "justify-center"
+              }`}
+          onClick={() => handleChannelClick()}
+
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -153,20 +155,17 @@ function SideFullNavBar({ isOpen, onToggle }) {
             />
           </svg>
 
-          {isOpen && (
-            <span
-              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${
-                isOpen ? "opacity-100 w-auto ml-2" : "opacity-0 w-0"
+          <span
+            className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${isOpen ? "w-auto ml-2" : "w-auto text-xs"
               }`}
-            >
-              Collections
-            </span>
-          )}
+          >
+            Collections
+          </span>
         </div>
-        <div
-          className={`flex items-center gap-4 py-2 ${
-            !isOpen && "justify-center"
-          }`}
+        <Link to="/subscribers" >
+        <div 
+            className={`flex ${isOpen ? "flex-row items-center gap-4" : "flex-col items-center gap-1 "} }  py-2 px-1 hover:bg-[#242424] rounded-xl ${!isOpen && "justify-center"
+              }`}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -183,16 +182,14 @@ function SideFullNavBar({ isOpen, onToggle }) {
             />
           </svg>
 
-          {isOpen && (
-            <span
-              className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${
-                isOpen ? "opacity-100 w-auto ml-2" : "opacity-0 w-0"
+          <span
+            className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${isOpen ? "w-auto ml-2" : "w-auto text-xs"
               }`}
-            >
-              Subscribers
-            </span>
-          )}
+          >
+            Subscribers
+          </span>
         </div>
+        </Link>
       </nav>
     </div>
   );
