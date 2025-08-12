a41fdc2b7840f04416759edfcc7915cf57cafda2
Abhinav-378 <abhichoukse05@gmail.com>
2025-08-12 16:05:51 +0530

    add loading skeleton for video cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import VideoCardSkeleton from './VideoCardSkeleton';
 
 function Home() {
   const API_URL =
@@ -34,47 +35,66 @@ function Home() {
     if (diff < 31536000) return `${Math.floor(diff / 2592000)} months ago`;
     return `${Math.floor(diff / 31536000)} years ago`;
   }
-  
+
   useEffect(() => {
     fetchRandomVideos()
   }, [])
   return (
     <div className='bg-[#0f0f0f] w-full h-full p-4'>
       <div>
-        {videos.length === 0 ?
-        <div className='flex justify-center items-center h-48 w-full '>
-            No videos uploaded yet
-        </div> :
-        <div className='flex flex-col md:flex-row flex-wrap items-start justify-start w-full mt-5'>
-            {
-                videos.map((video)=>(
-                  <div  className='flex flex-col w-full md:w-1/3 justify-start items-start gap-1 py-4 md:px-4  ' key={video._id}>
-                    <Link to={`/watch/${video._id}`} key={video._id}>
-                        <div className='relative'>
-                            <img src={video.thumbnail} alt={video.title} className=" rounded-lg w-dvw h-auto  " />
-                            <div className='absolute bottom-2 right-2 rounded-md bg-black/70 text-white px-2 py-1 text-sm'>{video.duration}</div>          
-                        </div>
-                        <div className="channel-info">
-                            <h2 className="text-base text-white line-clamp-2 mt-2">{video.title}</h2>
-                        </div>
-                    </Link>
-                    <Link to={`/channel/${video.owner.username}`} >
-                      <div className='text-sm text-gray-400 hover:text-gray-200'>
-                      {video.owner.fullName}
-                      </div>
-                    </Link>
-                    <Link to={`/watch/${video._id}`} >
-                      <div className='text-sm text-gray-400'>
-                      {video.views} views • {timeAgo(video.createdAt)}
-                      </div>
-                    </Link>
-                    </div>
-                    )
-                )
-            }
-        </div>
+        {
+          loading && (
+            // display 9 skeletons
+            <div className='flex flex-row flex-wrap items-center justify-center w-full mt-5'>
+              {Array.from({ length: 9 }).map((_, index) => (
+                <div className='gap-1 py-4 md:px-4' key={index}>
+                  <VideoCardSkeleton />
+                </div>
+              ))}
+            </div>
+          )
         }
-        </div>
+        {videos.length === 0 ?
+          <div className='flex justify-center items-center h-48 w-full '>
+            No videos uploaded yet
+          </div> :
+          <div className='flex flex-col md:flex-row flex-wrap items-start justify-start w-full mt-5'>
+            {
+              videos.map((video) => (
+                <div className='flex flex-col w-full md:w-1/3 justify-start items-start gap-1 py-4 md:px-4  ' key={video._id}>
+                  <Link className='w-full' to={`/watch/${video._id}`} key={video._id}>
+                    <div className='relative aspect-video w-full'>
+                      <img
+                        src={video.thumbnail}
+                        alt={video.title}
+                        className="rounded-lg w-full h-full object-cover"
+                      />
+                      <div className='absolute bottom-2 right-2 rounded-md bg-black/70 text-white px-2 py-1 text-sm'>
+                        {video.duration}
+                      </div>
+                    </div>
+
+                    <div className="channel-info">
+                      <h2 className="text-base text-white line-clamp-2 mt-2">{video.title}</h2>
+                    </div>
+                  </Link>
+                  <Link to={`/channel/${video.owner.username}`} >
+                    <div className='text-sm text-gray-400 hover:text-gray-200'>
+                      {video.owner.fullName}
+                    </div>
+                  </Link>
+                  <Link to={`/watch/${video._id}`} >
+                    <div className='text-sm text-gray-400'>
+                      {video.views} views • {timeAgo(video.createdAt)}
+                    </div>
+                  </Link>
+                </div>
+              )
+              )
+            }
+          </div>
+        }
+      </div>
     </div>
   )
 }
