940aebb879fa34575a3c484adc3ce25972dedbcb
Abhinav-378 <abhichoukse05@gmail.com>
2025-08-14 12:49:58 +0530

    added profile pic display if user logged in

--- src/components/SideFullNavBar.jsx
+++ src/components/SideFullNavBar.jsx
@@ -219,20 +219,29 @@ function SideFullNavBar({ isOpen, onToggle }) {
             <div
               className={`flex ${isOpen ? "flex-row items-center gap-4" : "flex-col items-center gap-1"} py-2 px-1 hover:bg-[#242424] rounded-xl ${!isOpen && "justify-center"}`}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-7"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-                />
-              </svg>
+              {
+                user ?
+                <img 
+                  src={user?.avatar} 
+                  alt="user" 
+                  className='w-8 h-8 rounded-full shadow-md shadow-purple-500/10 cursor-pointer'
+                />:
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-7"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                  />
+                </svg>
+              }
+              
 
               <span
                 className={`whitespace-nowrap overflow-hidden transition-all duration-300 ${isOpen ? "w-auto ml-2" : "w-auto text-xs"}`}
